aeb6a4e8306be1f7329c46687db732b602107d51
pppmmmqqq <2674264426@qq.com>
2020-09-09 23:06:13 +0800

    todolist五毛钱特效制作

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import TodoList from './TodoList';
-function App() {
-  return (
-    <div>
-      <TodoList />
-    </div>
-  );
+import React, { Component, Fragment } from 'react';
+import './style.css'
+import { CSSTransition,TransitionGroup } from 'react-transition-group'
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            list:[]
+        }
+        this.handleAddItem = this.handleAddItem.bind(this)
+    }
+    render() { 
+        return ( 
+            <Fragment>
+                <TransitionGroup>
+                {
+                    this.state.list.map((item, index) => {
+                        return (
+                            <CSSTransition
+                            timeout={1000}
+                            classNames='fade'
+                            unmountOnExit
+                            onEntered={(el)=>{el.style.color='pink'}}
+                            appear={true}
+                            key={index}
+                        >
+                            <div>{item}</div>
+                            </CSSTransition>
+                        )
+                    })
+                }
+                <button onClick={this.handleAddItem}>toggle</button>
+                </TransitionGroup>
+            </Fragment>
+         );
+    }
+    handleAddItem(){
+        this.setState((prevState)=>{
+            return {
+                list: [...prevState.list, 'item']
+            }
+        })
+    }
 }
-
+ 
 export default App;
