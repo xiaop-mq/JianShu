77ae6e568c23945785893821096e839081e39293
pppmmmqqq <2674264426@qq.com>
2020-09-09 23:40:52 +0800

    使用antd实现简单的UI图

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import TodoList from './TodoList';
-function App() {
-  return (
-    <div>
-      <TodoList />
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd'
 
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];  
+
+class App extends Component {
+    render() { 
+        return ( 
+            <div style={{marginTop: '10px', marginLeft: '10px'}}>
+                <Input placeholder="基本使用" style={{width: '300px', marginRight:'10px'}}/>
+                <Button type="primary">提交</Button>
+                <List
+                    style={{width:'320px', marginTop: '10px'}}
+                    size="small"
+                    bordered
+                    dataSource={data}
+                    renderItem={item => <List.Item>{item}</List.Item>}
+                    />
+            </div>
+         );
+    }
+}
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+import App from './App';
 
 ReactDOM.render(
-    <TodoList />,
+    <App />,
   document.getElementById('root')
 );
 
